048cb993d59cfdbb8834f8bfd4ccae3ec8613694
Christian WALDBILLIG <christian@waldbillig.io>
2023-04-01 11:43:32 +0000

    updates

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+REGISTRY=("ghcr.io")
+REPOPATH=("chrecht")
+REPOIMAGE=("docker-library/php")
+
+function buildAndPush()
+{
+
+    DOCKERFILE=$1
+    CONTEXT=$2
+    VARIANT=$3
+    MAJOR=$4
+    MINOR=$5
+
+	docker buildx build \
+		--platform=linux/amd64 \
+		--push \
+		--pull \
+		-t ${REGISTRY}/${REPOPATH}/${REPOIMAGE}:${MAJOR}-${VARIANT} \
+		-t ${REGISTRY}/${REPOPATH}/${REPOIMAGE}:${MAJOR}.${MINOR}-${VARIANT} \
+		-f ${DOCKERFILE} --progress=plain ${CONTEXT}
+
+}
+
+
+
+for i in "${!REGISTRY[@]}"
+do
+    
+    buildAndPush ./php/8.2/Dockerfile-apache ./php/8.2/ apache 8.2 4
+
+done
