fe0438c007fed7f2ec6a35cc46342e9e9492396c
Christian WALDBILLIG <christian@waldbillig.io>
2023-04-01 13:47:23 +0000

    ok

--- php/8.2/scripts/dev.sh
+++ php/8.2/scripts/dev.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 set -e
 
-pecl install xdebug
+phpenmod -v 8.2 xdebug
 
-rm -rf /usr/local/etc/php/conf.d/zzz_debug.ini || true
-echo "opcache.enable=0" >> /usr/local/etc/php/conf.d/zzz_debug.ini
-echo "opcache.enable_cli=0" >> /usr/local/etc/php/conf.d/zzz_debug.ini
-echo "zend_extension=xdebug.so" >> /usr/local/etc/php/conf.d/zzz_debug.ini
-echo "xdebug.mode=develop" >> /usr/local/etc/php/conf.d/zzz_debug.ini
-echo "display_errors=1" >> /usr/local/etc/php/conf.d/zzz_debug.ini
-echo "display_startup_errors=1" >> /usr/local/etc/php/conf.d/zzz_debug.ini
-echo "log_errors=1" >>  /usr/local/etc/php/conf.d/zzz_debug.ini
-echo "expose_php=1" >>  /usr/local/etc/php/conf.d/zzz_debug.ini
+rm -rf /etc/php/8.2/apache2/conf.d/zzz_debug.ini || true
+echo "opcache.enable=0" >> /etc/php/8.2/apache2/conf.d/zzz_debug.ini
+echo "opcache.enable_cli=0" >> /etc/php/8.2/apache2/conf.d/zzz_debug.ini
+echo "xdebug.mode=develop" >> /etc/php/8.2/apache2/conf.d/zzz_debug.ini
+echo "display_errors=1" >> /etc/php/8.2/apache2/conf.d/zzz_debug.ini
+echo "display_startup_errors=1" >> /etc/php/8.2/apache2/conf.d/zzz_debug.ini
+echo "log_errors=1" >>  /etc/php/8.2/apache2/conf.d/zzz_debug.ini
+echo "expose_php=1" >>  /etc/php/8.2/apache2/conf.d/zzz_debug.ini
