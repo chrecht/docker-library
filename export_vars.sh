5196e617221e4dab56ab3ff88e885d7e385af381
Christian WALDBILLIG <christian@waldbillig.io>
2023-04-04 07:24:38 +0000

    move to sh script

---
+++ export_vars.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+export MAJOR=$(curl --silent https://packages.sury.org/php/dists/bullseye/main/binary-amd64/Packages | grep "Package: php8.2-cli$" -A 5 | grep Version | sed -En  's/^Version: ([0-9]+\.[0-9]+).*/\1/p')
+export MINOR=$(curl --silent https://packages.sury.org/php/dists/bullseye/main/binary-amd64/Packages | grep "Package: php8.2-cli$" -A 5 | grep Version | sed -En  's/^Version: [0-9]+\.[0-9]+\.([0-9]+).*/\1/p')
