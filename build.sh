7f572437a89b2f9a79ecc14020f49bba1e1d891e
Christian WALDBILLIG <christian@waldbillig.io>
2023-04-04 06:48:41 +0000

    refactoring & cli

--- build.sh
+++ build.sh
@@ -28,12 +28,36 @@ function buildAndPush()
 
 }
 
+function build()
+{
+
+    DOCKERFILE=$1
+    CONTEXT=$2
+    VARIANT=$3
+    MAJOR=$4
+    MINOR=$5
+
+	REGISTRY=$6
+	REPOPATH=$7
+	REPOIMAGE=$8
+
+	docker buildx build \
+		--platform=linux/amd64 \
+		--push \
+		--pull \
+		-t ${REGISTRY}/${REPOPATH}/${REPOIMAGE}:${MAJOR}-${VARIANT} \
+		-t ${REGISTRY}/${REPOPATH}/${REPOIMAGE}:${MAJOR}.${MINOR}-${VARIANT} \
+		-f ${DOCKERFILE} --progress=plain ${CONTEXT}
+
+}
 
 
-for i in "${!REGISTRY[@]}"
-do
+build ./php/8.2/cli/Dockerfile ./php/8.2/cli cli 8.2 4 ${REGISTRY[$i]} ${REPOPATH[$i]} ${REPOIMAGE[$i]}
+
+# for i in "${!REGISTRY[@]}"
+# do
     
-    buildAndPush ./php/8.2/Dockerfile-apache ./php/8.2/ apache 8.2 4 ${REGISTRY[$i]} ${REPOPATH[$i]} ${REPOIMAGE[$i]}
-	buildAndPush ./php/8.2/Dockerfile-cli ./php/8.2/ cli 8.2 4 ${REGISTRY[$i]} ${REPOPATH[$i]} ${REPOIMAGE[$i]}
+#     buildAndPush ./php/8.2/Dockerfile-apache ./php/8.2/ apache 8.2 4 ${REGISTRY[$i]} ${REPOPATH[$i]} ${REPOIMAGE[$i]}
+# 	buildAndPush ./php/8.2/Dockerfile-cli ./php/8.2/ cli 8.2 4 ${REGISTRY[$i]} ${REPOPATH[$i]} ${REPOIMAGE[$i]}
 
-done
+# done
---
+++ php/8.2/cli/scripts/dev.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+
+echo "opcache.enable=0" >> /etc/php/8.2/cli/conf.d/zzz_debug.ini
+echo "opcache.enable_cli=0" >> /etc/php/8.2/cli/conf.d/zzz_debug.ini
+echo "xdebug.mode=develop" >> /etc/php/8.2/cli/conf.d/zzz_debug.ini
+echo "display_errors=1" >> /etc/php/8.2/cli/conf.d/zzz_debug.ini
+echo "display_startup_errors=1" >> /etc/php/8.2/cli/conf.d/zzz_debug.ini
+echo "log_errors=1" >>  /etc/php/8.2/cli/conf.d/zzz_debug.ini
+echo "expose_php=1" >>  /etc/php/8.2/cli/conf.d/zzz_debug.ini
