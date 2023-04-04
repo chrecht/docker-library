c9acc725d107630e3ebbe9096109b15ce2bbc048
Christian WALDBILLIG <christian@waldbillig.io>
2023-04-04 22:30:29 +0000

    ci

--- check-version-and-create-tag.sh
+++ check-version-and-create-tag.sh
@@ -22,14 +22,11 @@ if is_latest; then
     echo "Already lastest version; nothing to do."
 else
 
-    git remote add api-origin https://oauth2:${GITLAB_ACCESS_TOKEN}@gitlab.waldbillig.io/containers/docker-library.git
-    git pull --tags
-
     echo ${latest} > .current
     cat .current
 
-    #BRANCH_NAME="update-${CI_PROJECT_NAME}-${CI_PIPELINE_ID}"
-    #git checkout -b "${BRANCH_NAME}"
+    BRANCH_NAME="php-${latest}-${CI_PIPELINE_ID}"
+    git checkout -b "${BRANCH_NAME}"
     git config user.email "${GITLAB_USER_EMAIL}"
     git config user.name "${GITLAB_USER_NAME}"
 
@@ -40,8 +37,8 @@ else
     https://www.php.net/ChangeLog-8.php#${latest}
 EOF
 
-    git tag -a $MAJOR.$MINOR -m "Version created by gitlab-ci Build"
-    git push api-origin $MAJOR.$MINOR
+    git remote add api-origin https://oauth2:${GITLAB_ACCESS_TOKEN}@gitlab.waldbillig.io/containers/docker-library.git
+    git push api-origin ${BRANCH_NAME}
 
 fi
 
