f5ce9bd355bc64db78a4b0ca7fc931638545e4bd
Christian WALDBILLIG <christian@waldbillig.io>
2023-04-11 20:37:59 +0200

    ci

--- check-version-and-create-tag.sh
+++ check-version-and-create-tag.sh
@@ -8,15 +8,54 @@ get_latest_php_version()
 
 	latest=$(curl --silent https://packages.sury.org/php/dists/bullseye/main/binary-amd64/Packages | grep "Package: php${SEARCH_MAJOR}-cli$" -A 5 | grep Version | sed -En  's/^Version: ([0-9]+\.[0-9]+\.[0-9]+).*/\1/p')
 
-	ver_major=$(echo $latest | sed -En 's/^([0-9]+)\.[0-9]+\.[0-9]+/\1/p')
-	ver_minor=$(echo $latest | sed -En 's/^[0-9]+\.([0-9]+)\.[0-9]+/\1/p')
-	ver_patch=$(echo $latest | sed -En 's/^[0-9]+\.[0-9]+\.([0-9]+)/\1/p')
+	MAJOR=$(echo $latest | sed -En 's/^([0-9]+)\.[0-9]+\.[0-9]+/\1/p')
+	MINOR=$(echo $latest | sed -En 's/^[0-9]+\.([0-9]+)\.[0-9]+/\1/p')
+	PATCH=$(echo $latest | sed -En 's/^[0-9]+\.[0-9]+\.([0-9]+)/\1/p')
 
 }
 
+is_latest() {
+
+	SEARCH_MAJOR=$1
+
+    current=$(cat php/${SEARCH_MAJOR}/.env)
+
+    [[ ${latest} = ${current} ]]
+
+}
+
+
 get_latest_php_version 8.2
 
-rm php/8.2/.env || true
-echo "export MAJOR=${ver_major}" >> php/8.2/.env
-echo "export MINOR=${ver_minor}" >> php/8.2/.env
-echo "export PATCH=${ver_patch}" >> php/8.2/.env
+if is_latest 8.2; then
+
+	echo "Version is up to date"
+
+else
+
+	echo $latest > php/8.2/.current
+	echo "" > php/8.2/.env
+	echo "export MAJOR=${MAJOR}" >> php/8.2/.env
+	echo "export MINOR=${MINOR}" >> php/8.2/.env
+	echo "export PATCH=${PATCH}" >> php/8.2/.env
+
+
+    BRANCH_NAME="update-${CI_PROJECT_NAME}-${CI_PIPELINE_ID}"
+    git checkout -b "${BRANCH_NAME}"
+    git add php/8.2/.current
+	git add php/8.2/.env
+    git commit -F - <<EOF
+    Bump PHP version to ${latest}
+
+    https://www.php.net/ChangeLog-8.php#${latest}
+EOF
+
+    git config user.email "${GITLAB_USER_EMAIL}"
+    git config user.name "${GITLAB_USER_NAME}"
+    git remote remove origin
+    git remote add origin git@${CI_SERVER_HOST}:$CI_PROJECT_PATH.git
+    git tag -a ${MAJOR}.${MINOR}.${PATCH} -m "Version created by gitlab-ci"
+    git push origin ${MAJOR}.${MINOR}.${PATCH}
+
+
+fi
