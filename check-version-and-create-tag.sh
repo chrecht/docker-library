7b4af001b7088a043080a894bc841b8a594bbaee
Christian WALDBILLIG <christian@waldbillig.io>
2023-04-11 20:46:27 +0200

    ci

--- check-version-and-create-tag.sh
+++ check-version-and-create-tag.sh
@@ -44,6 +44,8 @@ else
 
 
     BRANCH_NAME="update-${CI_PROJECT_NAME}-${CI_PIPELINE_ID}"
+    git config user.email "${GITLAB_USER_EMAIL}"
+    git config user.name "${GITLAB_USER_NAME}"
     git checkout -b "${BRANCH_NAME}"
     git add php/8.2/.current
 	git add php/8.2/.env
@@ -53,8 +55,6 @@ else
     https://www.php.net/ChangeLog-8.php#${latest}
 EOF
 
-    git config user.email "${GITLAB_USER_EMAIL}"
-    git config user.name "${GITLAB_USER_NAME}"
     git remote remove origin
     git remote add origin git@${CI_SERVER_HOST}:$CI_PROJECT_PATH.git
     git tag -a ${MAJOR}.${MINOR}.${PATCH} -m "Version created by gitlab-ci"
