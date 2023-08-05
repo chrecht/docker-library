5171f34818f4d75f3e399bd1911a0e3a1c9d0a08
Christian WALDBILLIG <christian@waldbillig.io>
2023-08-05 17:15:06 +0000

    updated nginx & php

--- build-nginx.sh
+++ build-nginx.sh
@@ -57,6 +57,7 @@ BASEPATH=./nginx
 
 for i in "${!REGISTRY[@]}"
 do
-	buildAndPush ${BASEPATH}/1.24/Dockerfile ${BASEPATH}/1.24 mainline 1.24 0 ${REGISTRY[$i]} ${REPOPATH[$i]} ${REPOIMAGE[$i]}
-	buildAndPush ${BASEPATH}/1.23/Dockerfile ${BASEPATH}/1.23 stable 1.23 4 ${REGISTRY[$i]} ${REPOPATH[$i]} ${REPOIMAGE[$i]}
+	buildAndPush ${BASEPATH}/1.25/Dockerfile ${BASEPATH}/1.25 mainline 1.25 1 ${REGISTRY[$i]} ${REPOPATH[$i]} ${REPOIMAGE[$i]}
+	buildAndPush ${BASEPATH}/1.24/Dockerfile ${BASEPATH}/1.24 stable 1.24 0 ${REGISTRY[$i]} ${REPOPATH[$i]} ${REPOIMAGE[$i]}
+	buildAndPush ${BASEPATH}/1.23/Dockerfile ${BASEPATH}/1.23 old 1.23 4 ${REGISTRY[$i]} ${REPOPATH[$i]} ${REPOIMAGE[$i]}
 done
