c8520a864931fcd47817187e361e35c52ca305e1
Christian WALDBILLIG <christian@waldbillig.io>
2024-01-01 10:41:28 +0100

    nginx & tests

--- build-nginx.sh
+++ build-nginx.sh
@@ -57,6 +57,6 @@ BASEPATH=./nginx
 
 for i in "${!REGISTRY[@]}"
 do
-	buildAndPush ${BASEPATH}/1.24/Dockerfile ${BASEPATH}/1.24 mainline 1.24 0 ${REGISTRY[$i]} ${REPOPATH[$i]} ${REPOIMAGE[$i]}
-	buildAndPush ${BASEPATH}/1.23/Dockerfile ${BASEPATH}/1.23 stable 1.23 4 ${REGISTRY[$i]} ${REPOPATH[$i]} ${REPOIMAGE[$i]}
+	buildAndPush ${BASEPATH}/1.25/Dockerfile ${BASEPATH}/1.25 mainline 1.25 3 ${REGISTRY[$i]} ${REPOPATH[$i]} ${REPOIMAGE[$i]}
+	buildAndPush ${BASEPATH}/1.24/Dockerfile ${BASEPATH}/1.24 stable 1.24 0 ${REGISTRY[$i]} ${REPOPATH[$i]} ${REPOIMAGE[$i]}
 done
---
+++ tests/php-8.3/html/error.php
@@ -0,0 +1,3 @@
+<?php
+
+sjjsjsjs(;);
