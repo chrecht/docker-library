7d0cd6ecb5c42628858d8311f10eef616fb4494c
Christian WALDBILLIG <christian@waldbillig.io>
2026-01-10 19:38:54 +0100

    workflow updated & cli added

--- php/8.5/cli/scripts/dev.sh
+++ php/8.5/cli/scripts/dev.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -e
 
-echo "opcache.enable=0" >> /etc/php/8.5/cli/conf.d/zzz_debug.ini
-echo "opcache.enable_cli=0" >> /etc/php/8.5/cli/conf.d/zzz_debug.ini
-echo "xdebug.mode=develop" >> /etc/php/8.5/cli/conf.d/zzz_debug.ini
-echo "display_errors=1" >> /etc/php/8.5/cli/conf.d/zzz_debug.ini
-echo "display_startup_errors=1" >> /etc/php/8.5/cli/conf.d/zzz_debug.ini
-echo "log_errors=1" >>  /etc/php/8.5/cli/conf.d/zzz_debug.ini
-echo "expose_php=1" >>  /etc/php/8.5/cli/conf.d/zzz_debug.ini
+docker-php-ext-enable xdebug
+
+rm -rf /usr/local/etc/php/conf.d/zzz_debug.ini || true
+echo "opcache.enable=0" >> /usr/local/etc/php/conf.d/zzz_debug.ini
+echo "opcache.enable_cli=0" >> /usr/local/etc/php/conf.d/zzz_debug.ini
+echo "xdebug.mode=develop" >> /usr/local/etc/php/conf.d/zzz_debug.ini
+echo "display_errors=1" >> /usr/local/etc/php/conf.d/zzz_debug.ini
+echo "display_startup_errors=1" >> /usr/local/etc/php/conf.d/zzz_debug.ini
+echo "log_errors=1" >>  /usr/local/etc/php/conf.d/zzz_debug.ini
+echo "expose_php=1" >>  /usr/local/etc/php/conf.d/zzz_debug.ini
